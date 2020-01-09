dae813c4af5276f063ea8882cd72cfa4b7ebf38d
Luke Fox <l.fox9@nuigalway.ie>
2020-01-09 00:54:32 +0000

    componenent did mount method used to get dive information from api and display on page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
 import React, { Component } from "react";
 import styled, { css } from "styled-components";
 import HeaderX from "./components/Header/HeaderX";
+import ReactLoading from 'react-loading';
 import './App.css';
 
-function App() {
-    return (
-        <>
-            <HeaderX
-                style={{
-                    height: 80,
-                    marginTop: -3
-                }}
-            ></HeaderX>
-        </>
-    );
-}
 
-export default App;
+export default class App extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+                error: null,
+                isLoaded: false,
+                data : null,
+                diveId: 1793788534
+            };
+    }
+
+
+    componentDidMount(){
+        return fetch(`http://140.203.186.181:8050/dive-service/dives?diveId=${this.state.diveId}`, {
+            method: 'GET',
+        }).then((response) => response.json()).then((result) => {
+                this.setState({ isLoaded: true, data : result })
+                console.log(this.state.data)
+            }) ,
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+    }
+
+    render() {
+        const {error, isLoaded, items} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <HeaderX
+                        style={{
+                            height: 80,
+                            marginTop: -3
+                        }}
+                    ></HeaderX>
+                    <div>
+                        {/*component to display data*/}
+                        {this.state.data ? <ListComponent data={this.state.data}/> :
+                            <ReactLoading type={'spin'} color={'ffffff'} height={'20%'} width={'20%'}/>
+                        }
+                    </div>
+                </div>
+            );
+        }
+    }
+    }
+
+class ListComponent extends Component {
+    constructor(props){super(props)}
+    render() {
+        const diveInfo = Object.entries(this.props.data).map(([key,value])=>{
+            return (
+                <div>{key} : {value.toString()}</div>
+            );
+        })
+
+            return (
+                <div>{diveInfo}</div>
+            );
+        }
+    }
+
 
 const Rect = styled.div`
   height: 658px;
